5fee93e0327a9adaa2c5ce26521eb4c276bc9023
romeo adjam <sodangbe@users.noreply.github.com>
2021-01-05 00:53:25 -0600

    currently at step 4

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,6 +1,5 @@
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-
 import { Satellite } from '../satellite';
 
 @Component({
@@ -10,7 +9,17 @@ import { Satellite } from '../satellite';
 })
 export class OrbitListComponent implements OnInit {
   @Input() satellites: Satellite[];
-  constructor() { }
+  constructor() {
+
+    this.satellites = [
+      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+   ];
+
+   }
 
   ngOnInit() {
 
