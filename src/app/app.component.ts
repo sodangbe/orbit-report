20754a141b18562bb184edab77cfd8b78cf22d5f
romeo adjam <sodangbe@users.noreply.github.com>
2021-01-05 23:09:21 -0600

    finished assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,39 +8,33 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
+  displayList: Satellite[];
   sourceList: Satellite[];
   constructor() {
-    // this.sourceList = [
-    //    new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-    //    new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-    //    new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-    //    new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-    //    new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    // ];
     this.sourceList = [];
-   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
-
-   window.fetch(satellitesUrl).then(function(response) {
-      response.json().then(function(data) {
-
-         let fetchedSatellites = data.satellites;
-         // TODO: loop over satellites
-         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-         for(let i = 0; i < fetchedSatellites.length; i++){
-              let satellite = {
-            name: fetchedSatellites[i].name,
-            type: fetchedSatellites[i].type,
-            launchDate: fetchedSatellites[i].launchDate,
-            orbitType: fetchedSatellites[i].orbitType,
-            operational: fetchedSatellites[i].operational
-          };
-          this.sourceList.push(satellite);
-        };
-        this.displayList = this.sourceList.slice(0);
-        //};
-      }.bind(this));
-   }.bind(this));
-
+    this.displayList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          for (let i = 0; i < fetchedSatellites.length; i++) {
+            this.sourceList.push(new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational))
+          }
+          this.displayList = this.sourceList.slice(0);
+       }.bind(this));
+    }.bind(this));
  }
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  this.displayList = matchingSatellites;
+}
 }
--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,6 +1,5 @@
-import { Input } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
-import { Satellite } from '../satellite';
+import { Component, OnInit, Input } from '@angular/core';
+import {Satellite} from '../satellite'
 
 @Component({
   selector: 'app-orbit-list',
@@ -8,13 +7,19 @@ import { Satellite } from '../satellite';
   styleUrls: ['./orbit-list.component.css']
 })
 export class OrbitListComponent implements OnInit {
-  @Input() satellites: Satellite[];
-  constructor() {
-
-   }
+@Input() satellites: Satellite[];
+  constructor() { }
 
   ngOnInit() {
-
+  }
+  sort(column: string): void {
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
  }
-
 }
